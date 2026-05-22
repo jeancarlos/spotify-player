3ff32572074c34e4fcfa44e9603ff7653c841b88
Jean Souza <jeancarlos@users.noreply.github.com>
2026-05-22 01:08:20 -0300

    feat: configurar react-i18next com locales pt-BR e en-US

---
+++ src/lib/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import ptBR from '@/locales/pt-BR.json'
+import enUS from '@/locales/en-US.json'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'pt-BR',
+    supportedLngs: ['pt-BR', 'en-US'],
+    resources: {
+      'pt-BR': { translation: ptBR },
+      'en-US': { translation: enUS },
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+      lookupLocalStorage: 'spoter_lang',
+    },
+    interpolation: { escapeValue: false },
+  })
+
+export default i18n
